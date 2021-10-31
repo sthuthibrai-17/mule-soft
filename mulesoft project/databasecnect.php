d65b973a9372f0cca398474fa66ff6c1c4b627b7
sthuthibrai-17 <93384896+sthuthibrai-17@users.noreply.github.com>
2021-11-01 00:00:12 +0530

    Add files via upload

---
+++ mulesoft project/databasecnect.php
@@ -0,0 +1,10 @@
+s (7 sloc)  177 Bytes
+   
+<?php
+$host = "localhost";
+$username = "root";
+$password = "";
+$db = "moviesdb";
+$conn = mysqli_connect($host, $username, $password, $db) or die("Connection Failed");
+
+?>
